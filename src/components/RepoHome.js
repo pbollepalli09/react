a9c87b93e91aa10b4a50d33f520ee86ddcae2851
pbollepalli <pbollepalli@asurion.com>
2023-07-10 10:54:17 -0400

    RepoHome changes check in

--- src/components/RepoHome.js
+++ src/components/RepoHome.js
@@ -13,7 +13,6 @@
 
         const getRepoData = async () => {
             try{
-
                 let date = new Date().getTime();
                 let oneDay = 24*3600*1000;
                 let yest =  date-oneDay;
@@ -31,30 +30,30 @@
                 console.error(`fetch error: ${error}`)
             }
         };
-                return(
-                    <div className="repository-home">
-                        <div>
-                            <h1>Welcome to {name} Repository Home</h1>
 
-                            <div className="profile-card">
-                                <div className="repository-image">
-                                    <img src={avatar} alt={name} height="200" width="200"/>
-                                </div>
-
-                                <div className="profile-content">
-                                    <h2>Name: {name}</h2>
-                                    <h3>Url: {htmlUrl}</h3>
-                                    <h3>Starts: {stars}</h3>
-                                </div>
-
-                            </div>
-
-                            <div className="commitlist-wrapper">
-                                {commits.map((commit, index) =>
-                                    <CommitCard commit={commit} index={index} key={commit.sha} />,
-                                )}
-                            </div>
+        return(
+            <div className="repository-home">
+                <div>
+                    <h1>Welcome to "{name}" repository home</h1>
+                    <div className="profile-card">
+                        <div className="repository-image">
+                            <img src={avatar} alt={name} height="200" width="200"/>
                         </div>
+
+                        <div className="profile-content">
+                            <h2>Name: {name}</h2>
+                            <h3>Url: {htmlUrl}</h3>
+                            <h3>Stars: {stars}</h3>
+                        </div>
+
                     </div>
-                    )
-               }
+
+                    <div className="commitlist-wrapper">
+                        {commits.map((commit, index) =>
+                            <CommitCard commit={commit} index={index} key={commit.sha} />,
+                        )}
+                    </div>
+                </div>
+            </div>
+        )
+        }
